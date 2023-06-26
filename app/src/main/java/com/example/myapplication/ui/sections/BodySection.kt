00502ae271fc23fe62dcea473bd7b2719a4d9b64
Juan Vilada <jvillad1@gmail.com>
2023-06-26 18:49:31 -0500

    feat: BodySection.kt adjustments (play around)

--- app/src/main/java/com/example/myapplication/ui/sections/BodySection.kt
+++ app/src/main/java/com/example/myapplication/ui/sections/BodySection.kt
@@ -1,9 +1,16 @@
 package com.example.myapplication.ui.sections
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.domain.myscreen.model.BodyRow
 import com.example.myapplication.domain.myscreen.model.CrossSelling
 import com.example.myapplication.domain.myscreen.model.Message
@@ -16,25 +23,28 @@ fun BodySection(
 ) {
     if (body?.isNotEmpty() == true) {
         LazyColumn(
-            modifier = modifier
+            modifier = modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             body.map { bodyRow ->
                 when (bodyRow) {
                     is CrossSelling -> item {
                         Text(
-                            text = bodyRow.text
+                            text = bodyRow.text, textAlign = TextAlign.Center, modifier = Modifier.width(150.dp)
                         )
                     }
 
                     is Message -> item {
                         Text(
-                            text = bodyRow.text
+                            text = bodyRow.text, textAlign = TextAlign.Center, modifier = Modifier.width(150.dp)
                         )
                     }
 
                     is Section -> item {
                         Text(
-                            text = bodyRow.sectionTitle
+                            text = bodyRow.sectionTitle, textAlign = TextAlign.Center, modifier = Modifier.width(150.dp)
                         )
                     }
                 }
