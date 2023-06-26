7b66b6a446234240534fefc5f4da59b0a16b7ffb
Juan Camilo Villada <jvillad1@gmail.com>
2023-06-25 23:28:15 -0500

    feat: create HeaderSection and FooterSection composables

--- app/src/main/java/com/example/myapplication/ui/myscreen/MyScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/myscreen/MyScreen.kt
@@ -1,9 +1,15 @@
 package com.example.myapplication.ui.myscreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -11,6 +17,7 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.myapplication.R
+import com.example.myapplication.domain.myscreen.model.Button
 
 @Composable
 fun MyScreen() {
@@ -20,7 +27,7 @@ fun MyScreen() {
     ConstraintLayout(
         modifier = Modifier.fillMaxSize()
     ) {
-        val (type, title, iconUrl) = createRefs()
+        val (type, header, footer) = createRefs()
 
         Text(
             text = uiState.myScreenModel?.type?.name.orEmpty(),
@@ -30,25 +37,69 @@ fun MyScreen() {
                 end.linkTo(parent.end)
             }
         )
-        Text(
-            text = uiState.myScreenModel?.header?.title.orEmpty(),
-            modifier = Modifier.constrainAs(title) {
+
+        HeaderSection(
+            title = uiState.myScreenModel?.header?.title ?: "Default title",
+            iconUrl = uiState.myScreenModel?.header?.iconUrl,
+            modifier = Modifier.constrainAs(header) {
                 top.linkTo(type.bottom, margin = 16.dp)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
             }
         )
+
+        FooterSection(
+            buttonList = uiState.myScreenModel?.footer?.buttonList,
+            modifier = Modifier.constrainAs(footer) {
+                bottom.linkTo(parent.bottom, margin = 16.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        )
+    }
+}
+
+@Composable
+fun HeaderSection(
+    title: String,
+    iconUrl: String?,
+    modifier: Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+        )
         AsyncImage(
-            model = uiState.myScreenModel?.header?.iconUrl,
+            model = iconUrl,
             contentDescription = "Header icon url",
             placeholder = painterResource(R.drawable.ic_offline),
-            modifier = Modifier
-                .size(40.dp)
-                .constrainAs(iconUrl) {
-                    top.linkTo(title.bottom, margin = 16.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
+            modifier = Modifier.size(40.dp)
         )
+    }
+}
+
+@Composable
+fun FooterSection(
+    buttonList: List<Button>?,
+    modifier: Modifier
+) {
+    if (buttonList?.isNotEmpty() == true) {
+        Column(
+            modifier = modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            buttonList.map { button ->
+                Button(
+                    onClick = { TODO("Show a Snackbar here") }
+                ) {
+                    Text(text = button.label)
+                }
+            }
+        }
     }
 }
