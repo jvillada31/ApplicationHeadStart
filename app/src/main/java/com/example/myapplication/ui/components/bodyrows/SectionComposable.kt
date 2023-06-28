df3955cb8e68c71ae1860b448e693b1b8ba157cd
Juan Camilo Villada <jvillad1@gmail.com>
2023-06-28 10:06:53 -0500

    feat: create SectionComposable.kt

---
+++ app/src/main/java/com/example/myapplication/ui/components/bodyrows/SectionComposable.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication.ui.components.bodyrows
+
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SectionComposable(
+    sectionTitle: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .shadow(elevation = 16.dp)
+            .defaultMinSize(minHeight = 88.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+    ) {
+        Text(
+            text = sectionTitle,
+            modifier = Modifier.padding(top = 8.dp, start = 16.dp, bottom = 8.dp),
+            color = Color.Black.copy(alpha = 0.9f),
+            fontSize = 16.sp,
+            fontWeight = FontWeight(600),
+        )
+        Divider(
+            modifier = modifier,
+            thickness = 1.dp,
+            color = Color.LightGray
+        )
+    }
+}
--- app/src/main/java/com/example/myapplication/ui/sections/BodySection.kt
+++ app/src/main/java/com/example/myapplication/ui/sections/BodySection.kt
@@ -2,27 +2,19 @@ package com.example.myapplication.ui.sections
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.myapplication.domain.myscreen.model.BodyRow
 import com.example.myapplication.domain.myscreen.model.CrossSelling
 import com.example.myapplication.domain.myscreen.model.Message
 import com.example.myapplication.domain.myscreen.model.Section
+import com.example.myapplication.ui.components.bodyrows.SectionComposable
 
 @Composable
 fun BodySection(
@@ -57,25 +49,10 @@ fun BodySection(
                     }
 
                     is Section -> item {
-                        // Section(...)
-                        Card(
+                        SectionComposable(
+                            sectionTitle = bodyRow.sectionTitle,
                             modifier = modifier
-                                .fillMaxWidth()
-                                .shadow(elevation = 16.dp)
-                                .defaultMinSize(minHeight = 88.dp),
-                            shape = MaterialTheme.shapes.medium,
-                            colors = CardDefaults.cardColors(
-                                containerColor = Color.White
-                            ),
-                        ) {
-                            Text(
-                                text = bodyRow.sectionTitle,
-                                modifier = Modifier.padding(top = 8.dp, start = 16.dp),
-                                color = Color.Black.copy(alpha = 0.9f),
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight(600),
-                            )
-                        }
+                        )
                     }
                 }
             }
